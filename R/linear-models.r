d90a141fd30d7b0eab40c302e5ad0c0d72b4c785
Tand <tand@TanddeMacBook-Pro.local>
2018-09-07 12:03:12 -0400

    documenting data

--- R/linear-models.r
+++ R/linear-models.r
@@ -13,3 +13,13 @@ linear_model <- function(formula, data) {
   # Your code here.
   return(lm(formula = formula, data = data))
 }
+
+
+#' A data set for the first homework
+#'
+#' @name lm_patho
+#' @docType data
+#' @rdname lm_patho
+#' @description In homework 1 we explore the effect of numerical instability
+#' on linear regressions. This data set gives us a basis for exploration.
+NULL
