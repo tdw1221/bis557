5847fe013eae630af56e1250f40671f65f9626c4
Tand <tand@TanddeMacBook-Pro.local>
2018-09-10 00:44:53 -0400

    test

--- R/data-doc.r
+++ R/data-doc.r
@@ -9,3 +9,18 @@
 #' on linear regressions. This data set gives us a basis for exploration.
 NULL
 
+
+#' A data set for the first homework
+#'
+#' A dataset containing the prices and other attributes of almost 54,000
+#' diamonds.
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{http://www.diamondse.info/}
+"diamonds"
+
--- R/linear-models.r
+++ R/linear-models.r
@@ -15,12 +15,4 @@ linear_model <- function(formula, data) {
 }
 
 
-#' A data set for the first homework
-#'
-#' @name lm_patho
-#' @alias lm_patho
-#' @docType data
-#' @rdname lm_patho
-#' @description In homework 1 we explore the effect of numerical instability
-#' on linear regressions. This data set gives us a basis for exploration.
-NULL
+
