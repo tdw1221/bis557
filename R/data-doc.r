a98a69b207f8400d547143fb4e48b18baf2bf553
Tand <tand@TanddeMBP.wireless.yale.internal>
2018-09-10 11:10:20 -0400

    test

--- R/data-doc.r
+++ R/data-doc.r
@@ -7,7 +7,7 @@
 #' @rdname lm_patho
 #' @description In homework 1 we explore the effect of numerical instability
 #' on linear regressions. This data set gives us a basis for exploration.
-'lm_patho'
+'lm_patho.rda'
 
 #' A data set for the first homework
 #'
